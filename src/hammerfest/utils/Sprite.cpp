7ee37fbb7539998769b03e020b37d59add0d9c16
alexis-puska <a.puska@ile-noire.com>
2017-06-04 15:43:31 +0200

    add parse information

--- src/hammerfest/utils/Sprite.cpp
+++ src/hammerfest/utils/Sprite.cpp
@@ -1,6 +1,5 @@
 #include "Sprite.h"
 
-//sprite
 #include "resources/sprite_animation.h"
 #include "resources/sprite_arrow.h"
 #include "resources/sprite_background_effect.h"
@@ -12,11 +11,7 @@
 #include "resources/sprite_map.h"
 #include "resources/sprite_objets.h"
 #include "resources/sprite_player.h"
-
-//image
 #include "resources/image_menu.h"
-
-//json descriptor
 #include "resources/json_image_parser.h"
 
 Sprite Sprite::m_instance = Sprite();
@@ -27,46 +22,41 @@ Sprite::Sprite() {
 	IMG_Init (IMG_INIT_PNG);
 	fprintf(stderr, "Init sprite system\n");
 
-	fprintf(stderr, "%s", json_image_parser_json);
-
+	//fprintf(stderr, "%s", json_image_parser_json);
 	Json::Reader reader;
 	Json::Value root;
-
+	Json::Value element;
 	SDL_Surface * backgroundSurface = IMG_LoadPNG_RW(SDL_RWFromMem(sprite_player_png, sprite_player_png_len));
 
 	std::string jsonString(json_image_parser_json, json_image_parser_json + sizeof json_image_parser_json / sizeof json_image_parser_json[0]);
 	reader.parse(jsonString, root);
+	int idx = 0;
 	for (int i = 0; i < root.size(); i++) {
-		fprintf(stderr, "test json : %i %i %s\n", root.size(), i, root[i]["file"].asCString());
-		fprintf(stderr, "nb area : %i\n", root[i]["area"].size());
 		for (int j = 0; j < root[i]["area"].size(); j++) {
-		fprintf(stderr, "area : %i %i %i %i %i %i %i %s %s\n", root[i]["area"][j]["x"].asUInt(),
-				root[i]["area"][j]["y"].asUInt(),
-				root[i]["area"][j]["nx"].asUInt(),
-				root[i]["area"][j]["ny"].asUInt(),
-				root[i]["area"][j]["n"].asUInt(),
-				root[i]["area"][j]["sx"].asUInt(),
-				root[i]["area"][j]["sy"].asUInt(),
-				root[i]["area"][j]["r"].asBool() ? "true" : "false",
-				root[i]["area"][j]["animation"].asCString()
-		);
+			element = root[i]["area"];
+			//fprintf(stderr, "area : %i %i %i %i %i %i %i %s %s\n", element[j]["x"].asUInt(), element[j]["y"].asUInt(), element[j]["nx"].asUInt(), element[j]["ny"].asUInt(),
+			//		element[j]["n"].asUInt(), element[j]["sx"].asUInt(), element[j]["sy"].asUInt(), element[j]["r"].asBool() ? "true" : "false", element[j]["animation"].asCString());
+			idx = 0;
+			for (int y = 0; y < element[j]["ny"].asUInt(); y++) {
+				for (int x = 0; x < element[j]["nx"].asUInt(); x++) {
+
+					idx++;
+				}
+			}
+		}
 	}
-}
-
-fprintf(stderr, "\n\n\n\nEnd init sprite system\n");
-
+	fprintf(stderr, "\n\n\n\nEnd init sprite system\n");
 }
 
 Sprite::~Sprite() {
-fprintf(stderr, "close sprite system\n");
-
-fprintf(stderr, "close font text system\n");
-//TTF_CloseFont(font);
-IMG_Quit();
-TTF_Quit();
+	fprintf(stderr, "close sprite system\n");
+	fprintf(stderr, "close font text system\n");
+	//TTF_CloseFont(font);‹‹
+	IMG_Quit();
+	TTF_Quit();
 }
 
 Sprite& Sprite::Instance() {
-return m_instance;
+	return m_instance;
 }
 
